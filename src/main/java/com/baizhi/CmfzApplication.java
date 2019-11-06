5e8c72d77f47939f909d58309112a61243ed41c0
suxin <xinsu_1119@163.com>
2019-11-06 18:07:04 +0800

    哈哈第7次玩这个

--- src/main/java/com/baizhi/CmfzApplication.java
+++ src/main/java/com/baizhi/CmfzApplication.java
@@ -15,7 +15,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 public class CmfzApplication {
 
     public static void main(String[] args) {
-        System.out.println("111111111111111111111111");
+        System.out.println("1111111111111111111");
         SpringApplication.run(CmfzApplication.class, args);
     }
 
